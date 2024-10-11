905247f38a3f57494d6fab13fbc5381f0b790347
Karthikeyan <165583967+Karthikeyan8296@users.noreply.github.com>
2024-10-11 19:36:44 +0530

    Initial Commit

---
+++ GlamMart/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 items-center justify-center">
+        <Text className="text-black text-xl">Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
