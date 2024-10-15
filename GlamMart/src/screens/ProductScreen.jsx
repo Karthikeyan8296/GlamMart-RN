493e0487f4a9d1599db2f0edfc6fea01083d3913
Karthikeyan <165583967+Karthikeyan8296@users.noreply.github.com>
2024-10-15 08:20:04 +0530

    Product screen done!

--- GlamMart/src/screens/ProductScreen.jsx
+++ GlamMart/src/screens/ProductScreen.jsx
@@ -11,6 +11,8 @@ import React, {useEffect, useState} from 'react';
 import {useSelector} from 'react-redux';
 import Icon from 'react-native-vector-icons/Entypo';
 import Icons from 'react-native-vector-icons/MaterialIcons';
+import {useNavigation} from '@react-navigation/native';
+import {ROUTES} from '../constants';
 
 const ProductScreen = ({route}) => {
   //by this we are getting the values of ID as params//
@@ -31,6 +33,20 @@ const ProductScreen = ({route}) => {
     }
   }, []);
 
+  const [isFavorite, setisFavorite] = useState(false);
+  const toggleFavorite = () => {
+    setisFavorite(!isFavorite);
+  };
+
+  const [Qty, setQty] = useState(1);
+  const handleQty = actions => {
+    const newQty = Qty + actions;
+    //this will set the user to dont run beyond the one!//
+    setQty(newQty >= 1 ? newQty : 1);
+  };
+
+  const navigation = useNavigation();
+
   //screen height//
   const screenHeight = Math.round(Dimensions.get('window').height);
 
@@ -45,7 +61,7 @@ const ProductScreen = ({route}) => {
           <SafeAreaView className="w-full">
             {/* header section */}
             <View className="flex-row items-center justify-between px-4 py-4 pt-6 w-full">
-              <TouchableOpacity>
+              <TouchableOpacity onPress={() => navigation.goBack()}>
                 <Icon name="chevron-left" size={32} color={'#555'} />
               </TouchableOpacity>
               <TouchableOpacity>
@@ -66,13 +82,75 @@ const ProductScreen = ({route}) => {
                 <Image
                   source={{uri: data?.mainImage?.asset?.url}}
                   resizeMode="contain"
-                  className="w-full h-full"
+                  className="w-80 h-80"
                 />
               </View>
             </View>
-
             {/* Category section */}
+            <View className="w-full flex-row items-center justify-evenly mb-2 mt-5">
+              {data?.categories &&
+                data?.categories?.length > 0 &&
+                data?.categories.map(value => (
+                  <View
+                    className="w-24 items-center justify-evenly p-2 rounded-xl bg-white"
+                    key={value._id}>
+                    <Image
+                      source={{uri: value?.mainImage?.asset?.url}}
+                      resizeMode="contain"
+                      className="w-10 h-10"
+                    />
+                    <Text className="font-semibold text-gray-500">
+                      {value.title}
+                    </Text>
+                  </View>
+                ))}
+            </View>
           </SafeAreaView>
+
+          {/* Bottom section */}
+          <View className="w-full flex-1 bg-white rounded-t-[36px] py-6 px-12 space-y-4">
+            <View className="w-full items-center justify-between flex-row">
+              <View>
+                <Text className="text-black font-semibold text-xl">
+                  {data?.title}
+                </Text>
+                <Text className=" text-stone-700">
+                  {data?.shortDescription}
+                </Text>
+              </View>
+              <TouchableOpacity onPress={toggleFavorite}>
+                <Icons
+                  name={isFavorite ? 'favorite' : 'favorite-border'}
+                  size={26}
+                  color={isFavorite ? 'red' : 'black'}
+                />
+              </TouchableOpacity>
+            </View>
+
+            <View className="w-full flex-row items-center justify-between">
+              <Text className="text-black font-extrabold text-xl">
+                ${data?.price}/-
+              </Text>
+
+              <View className="flex-row items-center justify-center space-x-4 rounded-xl border border-gray-300 px-4 py-1">
+                <TouchableOpacity onPress={() => handleQty(-1)}>
+                  <Text className="text-xl font-bold text-gray-900">-</Text>
+                </TouchableOpacity>
+                <Text className="text-xl font-bold text-gray-900">{Qty}</Text>
+                <TouchableOpacity onPress={() => handleQty(+1)}>
+                  <Text className="text-xl font-bold text-gray-900">+</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+
+            <TouchableOpacity
+              activeOpacity={0.8}
+              className="w-full h-14 rounded-xl bg-black items-center justify-center">
+              <Text className="text-white text-xl font-semibold">
+                Add to cart
+              </Text>
+            </TouchableOpacity>
+          </View>
         </>
       )}
     </View>
